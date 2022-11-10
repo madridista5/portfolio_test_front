ae79618aca1c56d89140b6b3404fe1d99a415922
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-11-10 11:55:05 +0100

    HTML structure of left side Intro's component has been created.

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -4,6 +4,23 @@ import './Intro.css';
 
 export const Intro = () => {
     return(
-        <div></div>
+        <div className="i">
+            <div className="i-left">
+                <div className="i-left-wrapper">
+                    <h2 className="i-intro">Hello, my name is</h2>
+                    <h1 className="i-name">John Lock</h1>
+                    <div className="i-title">
+                        <div className="i-title-wrapper">
+                            <div className="i-title-item">Web Developer</div>
+                            <div className="i-title-item">UI / UX Designer</div>
+                            <div className="i-title-item">Photographer</div>
+                            <div className="i-title-item">Writer</div>
+                            <div className="i-title-item">Content Creator</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="i-right">right</div>
+        </div>
     );
 }
