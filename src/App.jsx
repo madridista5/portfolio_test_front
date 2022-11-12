edbb61b649725cd50a065a572cb5aca1d09e30c9
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-11-12 12:12:34 +0100

    Component About has been created and finished.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {Intro} from "./components/Intro/Intro";
+import {About} from "./components/About/About";
 
 import './App.css';
 
@@ -7,6 +8,7 @@ export const App = () => {
     return (
         <div>
             <Intro/>
+            <About/>
         </div>
     );
 }
