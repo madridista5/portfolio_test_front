1ebb225f6664a736c25c67c89192299d8ca6e230
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-11-13 11:15:32 +0100

    Components ProductList and Products have been created.

---
+++ src/utils/data.js
@@ -0,0 +1,26 @@
+export const products = [
+    {
+        id: 1,
+        img: 'https://cdn.pixabay.com/photo/2015/04/04/20/42/code-707069_960_720.jpg',
+    },
+    {
+        id: 2,
+        img: 'https://cdn.pixabay.com/photo/2016/03/25/01/48/abstract-1278058_960_720.jpg',
+    },
+    {
+        id: 3,
+        img: 'https://cdn.pixabay.com/photo/2014/09/20/23/44/website-454460_960_720.jpg',
+    },
+    {
+        id: 4,
+        img: 'https://cdn.pixabay.com/photo/2016/03/25/01/43/abstract-1278044_960_720.jpg',
+    },
+    {
+        id: 5,
+        img: 'https://cdn.pixabay.com/photo/2016/03/25/01/46/abstract-1278055_960_720.jpg',
+    },
+    {
+        id: 6,
+        img: 'https://cdn.pixabay.com/photo/2019/08/06/12/37/girl-4388310_960_720.jpg',
+    },
+];
