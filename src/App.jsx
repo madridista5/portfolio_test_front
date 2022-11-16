29a227a76c01ced63961e4596fae2e9cd7f862e0
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-11-16 13:56:46 +0100

    Component Toggle has been created.

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import {Intro} from "./components/Intro/Intro";
 import {About} from "./components/About/About";
 import {ProductList} from "./components/ProductList/ProductList";
 import {Contact} from "./components/Contact/Contact";
+import {Toggle} from "./components/Toggle/Toggle";
 
 import './App.css';
 
 export const App = () => {
     return (
         <div>
+            <Toggle/>
             <Intro/>
             <About/>
             <ProductList/>
---
+++ src/components/Toggle/Toggle.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import './Toggle.css';
+
+export const Toggle = () => {
+    return(
+        <div className="t">toggle</div>
+    );
+}
