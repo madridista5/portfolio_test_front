4906e0fbdaf19ce81ebc079b9128282c667eb619
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-11-16 14:09:38 +0100

    Component Toggle has been finished.

--- src/components/Toggle/Toggle.jsx
+++ src/components/Toggle/Toggle.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import Sun from '../../images/sun.png';
+import Moon from '../../images/moon.png';
 
 import './Toggle.css';
 
 export const Toggle = () => {
     return(
-        <div className="t">toggle</div>
+        <div className="t">
+            <img src={Sun} alt="sun" className="t-icon"/>
+            <img src={Moon} alt="moon" className="t-icon"/>
+            <div className="t-button"></div>
+        </div>
     );
 }
