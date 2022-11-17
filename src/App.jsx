765bb7adca632ef4bc3572dd7465a0d58682bd93
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-11-17 21:05:42 +0100

    Function change theme (dark / light mode) has been finished.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, {useContext} from "react";
 import {Intro} from "./components/Intro/Intro";
 import {About} from "./components/About/About";
 import {ProductList} from "./components/ProductList/ProductList";
 import {Contact} from "./components/Contact/Contact";
 import {Toggle} from "./components/Toggle/Toggle";
+import {ThemeContext} from "./context/ThemeContext";
 
 import './App.css';
 
 export const App = () => {
+    const theme = useContext(ThemeContext);
+    const darkMode = theme.state.darkMode;
+
     return (
-        <div>
+        <div style={{backgroundColor: darkMode ? '#222' : 'white', color: darkMode && 'white'}}>
             <Toggle/>
             <Intro/>
             <About/>
